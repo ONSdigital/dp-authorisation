e1ce2713c2d9d099ce031988a0638cd813340f25
Dave Llewellyn <439837@gmail.com>
2019-07-09 12:11:16 +0100

    added unit tests for unmarshalPermissionsResponseEntity

--- authv2/permissions_client_test.go
+++ authv2/permissions_client_test.go
@@ -1,13 +1,14 @@
 package authv2
 
 import (
+	"encoding/json"
+	"fmt"
 	"testing"
 
 	. "github.com/smartystreets/goconvey/convey"
 )
 
 func TestPermissionsResponseEntityToPermissions(t *testing.T) {
-
 	testCases := []struct {
 		entity   *permissionsResponseEntity
 		expected *Permissions
@@ -52,5 +53,91 @@ func TestPermissionsResponseEntityToPermissions(t *testing.T) {
 			So(actual, ShouldResemble, tc.expected)
 		}
 	})
+}
 
+func TestUnmarshalPermissionsResponseEntity(t *testing.T) {
+	testCases := []struct {
+		scenario     string
+		getInput     func() []byte
+		assertEntity func(*permissionsResponseEntity)
+		assertError  func(error)
+	}{
+		{
+			scenario: "Given an empty byte array",
+			getInput: func() []byte {
+				return []byte{}
+			},
+			assertEntity: func(actual *permissionsResponseEntity) {
+				So(actual, ShouldResemble, &permissionsResponseEntity{})
+			},
+			assertError: func(err error) {
+				So(err, ShouldBeNil)
+			},
+		},
+		{
+			scenario: "Given an nil byte array",
+			getInput: func() []byte {
+				return nil
+			},
+			assertEntity: func(actual *permissionsResponseEntity) {
+				So(actual, ShouldResemble, &permissionsResponseEntity{})
+			},
+			assertError: func(err error) {
+				So(err, ShouldBeNil)
+			},
+		},
+		{
+			scenario: "Given an byte array that is not a valid permissionsResponseEntity",
+			getInput: func() []byte {
+				return []byte("I AM NOT VALID")
+			},
+			assertEntity: func(actual *permissionsResponseEntity) {
+				So(actual, ShouldBeNil)
+			},
+			assertError: func(err error) {
+				So(err, ShouldNotBeNil)
+				permErr, ok := err.(Error)
+				So(ok, ShouldBeTrue)
+				So(permErr.Status, ShouldEqual, 500)
+				So(permErr.Message, ShouldEqual, "internal server error failed marshalling permissions response entity")
+			},
+		},
+		{
+			scenario: "Given a byte array containing valid permissionsResponseEntity data",
+			getInput: func() []byte {
+				b, err := json.Marshal(&permissionsResponseEntity{
+					List: []permissionType{
+						Create, Read, Update, Delete},
+				})
+				So(err, ShouldBeNil)
+				return b
+			},
+			assertEntity: func(actual *permissionsResponseEntity) {
+				So(actual, ShouldResemble, &permissionsResponseEntity{
+					List: []permissionType{
+						Create, Read, Update, Delete},
+				})
+			},
+			assertError: func(err error) {
+				So(err, ShouldBeNil)
+			},
+		},
+	}
+
+	for i, tc := range testCases {
+		Convey(fmt.Sprintf("%d/%d) %s", i+1, len(testCases), tc.scenario), t, func() {
+
+			Convey("when unmarshalPermissionsResponseEntity is called", func() {
+				actual, err := unmarshalPermissionsResponseEntity(tc.getInput())
+
+				Convey("then the expected permissionsResponseEntity is returned", func() {
+					tc.assertEntity(actual)
+				})
+
+				Convey("and the expected error is returned", func() {
+					tc.assertError(err)
+				})
+			})
+		})
+	}
 }
