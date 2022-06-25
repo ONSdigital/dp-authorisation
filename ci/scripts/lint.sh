4068efbd393eac3e0843c3e56c1202a07ad57f2f
Geraint Edwards <gedgey@gmail.com>
2022-06-25 13:09:04 +0100

    download rather than build golangci-lint

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -2,7 +2,9 @@
 
 cwd=$(pwd)
 
-go install github.com/golangci/golangci-lint/cmd/golangci-lint@v1.46.2
+lint_ver=1.46.2
+curl -o - https://github.com/golangci/golangci-lint/releases/download/v$lint_ver/golangci-lint-$lint_ver-linux-amd64.tar.gz | tar zxvf -
+PATH=$PATH:$cwd/golangci-lint-$lint_ver-linux-amd64
 
 pushd $cwd/dp-authorisation/v2
   make lint
