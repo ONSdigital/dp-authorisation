9d4a302a9f0ff4669ba485718d52a561a18b60f0
Geraint Edwards <gedgey@gmail.com>
2022-06-25 13:15:14 +0100

    fix lint

--- ci/scripts/lint.sh
+++ ci/scripts/lint.sh
@@ -3,7 +3,7 @@
 cwd=$(pwd)
 
 lint_ver=1.46.2
-curl --no-progress-meter https://github.com/golangci/golangci-lint/releases/download/v$lint_ver/golangci-lint-$lint_ver-linux-amd64.tar.gz | tar zxvf -
+curl --location --no-progress-meter https://github.com/golangci/golangci-lint/releases/download/v$lint_ver/golangci-lint-$lint_ver-linux-amd64.tar.gz | tar zxvf -
 PATH=$PATH:$cwd/golangci-lint-$lint_ver-linux-amd64
 
 pushd $cwd/dp-authorisation/v2
